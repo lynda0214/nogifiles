d4c82ea545c18abaaffc95553e9dbc78639cbf23
Lynda <lynda0214@gmail.com>
2019-06-09 03:05:53 +0800

    Updates

---
+++ precache-manifest.d4bf58edb8d8d734eb44ae86b894fa35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68b724fb39b505b71ce4775b26f8f1a6",
+    "url": "/nogifiles/index.html"
+  },
+  {
+    "revision": "a1440cbb2a5162d4c679",
+    "url": "/nogifiles/static/css/main.37be2913.chunk.css"
+  },
+  {
+    "revision": "92b8a0ba9765bb388566",
+    "url": "/nogifiles/static/js/2.18797e6d.chunk.js"
+  },
+  {
+    "revision": "a1440cbb2a5162d4c679",
+    "url": "/nogifiles/static/js/main.6efa0c75.chunk.js"
+  },
+  {
+    "revision": "e47a15072804d9acd158",
+    "url": "/nogifiles/static/js/runtime~main.b40a7bc7.js"
+  }
+]);
